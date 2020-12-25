9532d94d5fdbcc52af8d1039e380dcc3a3b00219
Xiao <375986256@qq.com>
2020-12-25 15:30:47 +0800

    1.修正了生成人员姓名和性别、身份证的匹配问题，版本更新V1.1

--- Testdata/Person_make.py
+++ Testdata/Person_make.py
@@ -10,21 +10,27 @@ Phone=[]
 ID=[]
 Birthday=[]
 Sex=[]
-print("<==========人员信息随机生成系统v1.0============>")
 def Person_make():
+    print("<---------人员信息测试数据生成系统v1.1--------->")
     num=int(input("请输入您想生成人员采集的数量:"))
     Person=num
-    time.sleep(1.5)
+    time.sleep(1)
     print("请稍等,人员生成中………")
-    time.sleep(1.5)
+    time.sleep(1)
     start_time=time.time()
     for i in range(Person):
         if i<Person:
-            man = fake.name()
-            #生成男人的名字
+            # man = fake.name_male()
+            #生成男人的名字，有概率生成女人的名字，这个方法其实不是很好
             phone = fake.phone_number()
             #生成手机号码
             is_id = fake.ssn(min_age=19, max_age=45)
+            Date=is_id
+            sex_id=Date[16]
+            if int(sex_id) % 2 == 0:
+                man=Women_name()
+            else:
+                man=Man_name()
             #生成最小年龄为19岁，最大年龄为45岁的省份证号
             Name.append(str(man))
             Phone.append(str(phone))
@@ -40,11 +46,11 @@ def Person_make():
         date = date[13:15]
         birthday = year + '-' + month + '-' + date
         Birthday.append(str('\t')+birthday)
-        #提取身份证日期，然后输出为标准格式XXXX-XX-XX
-        #日期显示与身份证同理，\t
+        '''提取身份证日期，然后输出为标准格式XXXX-XX-XX
+        #日期显示与身份证同理，\t'''
     for y in ID:
-        Date = y
-        Sex_id = Date[17:18]
+        Date_1 = y
+        Sex_id = Date_1[17:18]
         if int(Sex_id) % 2 == 0:    #身份证男女校验，身份证第17位数为奇数为男，偶数为女
             sex="女"
             Sex.append(sex)
@@ -84,4 +90,24 @@ def Person_make():
     time.sleep(1)
     print('Running time: %.2f Seconds'%(end_time-start_time) + '\n')
     input("请按任意键退出,谢谢！")
+# Person_make()
+
+
+def Women_name():
+    name=fake.first_name()+fake.first_name_female()
+    return name
+        #姓氏方法+女性名称方法缝合,生成完美女性名字
+
+def Man_name():
+    name = fake.first_name() + fake.first_name_male()
+    return name
+    # 男性名称同理女性完美名称
+
+
 Person_make()
+# idd=[]
+# for m in range(100):
+#     Id=fake.ssn(min_age=19, max_age=45)
+#     idd.append(Id)
+# print(np.array(idd))
+
