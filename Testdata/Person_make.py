d82b460df383cbdda4180450421e49be40cd055a
Xiao <375986256@qq.com>
2020-12-25 15:39:06 +0800

    1.代码风格优化

--- Testdata/Person_make.py
+++ Testdata/Person_make.py
@@ -10,6 +10,17 @@ Phone=[]
 ID=[]
 Birthday=[]
 Sex=[]
+
+def Women_name():
+    name=fake.first_name()+fake.first_name_female()
+    return name
+        #姓氏方法+女性名称方法缝合,生成完美女性名字
+
+def Man_name():
+    name = fake.first_name() + fake.first_name_male()
+    return name
+    # 男性名称同理女性完美名称
+
 def Person_make():
     print("<---------人员信息测试数据生成系统v1.1--------->")
     num=int(input("请输入您想生成人员采集的数量:"))
@@ -90,24 +101,7 @@ def Person_make():
     time.sleep(1)
     print('Running time: %.2f Seconds'%(end_time-start_time) + '\n')
     input("请按任意键退出,谢谢！")
-# Person_make()
-
-
-def Women_name():
-    name=fake.first_name()+fake.first_name_female()
-    return name
-        #姓氏方法+女性名称方法缝合,生成完美女性名字
-
-def Man_name():
-    name = fake.first_name() + fake.first_name_male()
-    return name
-    # 男性名称同理女性完美名称
-
 
 Person_make()
-# idd=[]
-# for m in range(100):
-#     Id=fake.ssn(min_age=19, max_age=45)
-#     idd.append(Id)
-# print(np.array(idd))
+
 
