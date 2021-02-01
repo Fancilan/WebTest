da94a58aff8de824a309faf51617458aa2da7c1c
Xiao <375986256@qq.com>
2021-02-01 18:03:49 +0800

    update v1.1

--- Testdata/Vim_make.py
+++ Testdata/Vim_make.py
@@ -1,5 +1,6 @@
 import random
 import time
+import getpass
 def random_vin():
 # 内容的权值
 	content_map = {
@@ -23,13 +24,16 @@ def random_vin():
 	vin = ''.join(list1)
 	return vin
 
-def Vin_make():
-	print("<-----------汽车Vin码生成器v1.0----------->")
+def vin_make():
 	num=int(input("请输入您想要生成vin码的个数："))
 	time.sleep(1)
 	print("请稍等，生成中……")
-	file = open("VinData.txt", "w", encoding="UTF-8-sig")
+	cal = str(time.strftime("%Y-%m-%d", time.localtime())) + '-' + str(''.join(random.sample('0123456789', 5)))
+	user_name = getpass.getuser()  # 获取当前用户名
+	data_name = 'C:\\Users\\' + user_name + '\\Desktop\\vim_data_' + cal + '.txt'
+	file = open(data_name, "w", encoding="UTF-8-sig")
 	start_time = time.time()
+	origin_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 	for n in range(num):
 		if n<num:
 			res = random_vin()
@@ -39,10 +43,26 @@ def Vin_make():
 		else:
 			break
 	end_time = time.time()
+	final_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 	time.sleep(1)
 	print("导出成功Success！导出文件在根目录下")
 	print('Running time: %.2f Seconds' % (end_time - start_time) + '\n')
+	print('程序开始时间：' + origin_time)
+	print('程序结束时间：' + final_time)
 	file.close()
-	end=input("按任意键退出程序！")
+	time.sleep(1)
+	print("文件路径:" + data_name)
+	time.sleep(1.5)
+	end = int(input("请问还要继续生成VIN数据么？\n1.是\n2.否\n:"))
+	if end == 1:
+		vin_make()
+	else:
+		eend = input("请按任意键退出,谢谢！")
 
-Vin_make()
+def start():
+	print("<-----------汽车Vin码生成器v1.1----------->")
+	vin_make()
+
+
+
+start()
